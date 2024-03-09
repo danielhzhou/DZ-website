0966860513cf5b52d627249409e3f9eee5973c82
scrumptious snake <danielzh021@gmail.com>
2024-03-08 18:32:15 -0800

    new description

--- src/components/ProjectsA.js
+++ src/components/ProjectsA.js
@@ -13,14 +13,13 @@ function ProjectsA() {
             <ProjectsItem
               src="images/design-img1.jpg"
               text="
-               Leading a team of 6 members, we are currently 
-               working on a web app that will automatically 
-               create Youtube livestreams, thumbnails, and schedule
-                streams by reading a given csv file that holds 
-                information about the baseball games. We are also 
-                tasked with creating a working account system, 
-                secure hosting to the internet, and functions to 
-                delete and edit streams utilizing Youtube API."
+               Leading a team of 6 members, I am working on a web app and backend api utilizing React, Javascript, Flask, Python, Youtube API, and Google OAuth to automate 400+ streams/year for Riviera Little League. Translated livestreaming product requirements to technical specifications, designing a robust end-to-end system.
+               Utilized React, Javascript, Flask, and Python for development in order to best utilize the skills of my team to meet product requirements.
+               Developed frontend web interface and functionalities, including a file upload system (for little league schedule), adaptive stream display, and stream deletion features.
+               Integrated Google OAuth for user identification and worked with team members to create automatic thumbnail generator and automatic livestream creator with Youtube API.
+               Hosted frontend and backend on Amazon Web Services.
+               Implemented cybersecurity protocols to secure connections and protect sensitive data.
+               "
             />
           </ul>
         </div>
